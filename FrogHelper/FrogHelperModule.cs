3a5f1beafc5ba417ad567935c4e05f7497d773f9
Luna <izzeldeen03@gmail.com>
2021-08-02 18:41:23 +0100

    Add project files.

---
+++ FrogHelper/FrogHelperModule.cs
@@ -0,0 +1,15 @@
+﻿using Celeste.Mod;
+using System;
+
+namespace FrogHelper {
+	public class FrogHelperModule : EverestModule {
+
+		public override void Load() {
+			
+		}
+
+		public override void Unload() {
+			
+		}
+	}
+}
