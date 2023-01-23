72fc690d11cf961e168fffea48810e10a77409bc
Popax21 <popa21@popa21.at>
2023-01-23 18:27:29 +0100

    Fix frog shard cound position not saving

--- FrogHelper/FrogHelperSettings.cs
+++ FrogHelper/FrogHelperSettings.cs
@@ -5,19 +5,19 @@ using YamlDotNet.Serialization;
 
 namespace FrogHelper {
 	public class FrogHelperSettings : EverestModuleSettings {
-		[YamlMember]
-		private int frogShardCountPos = -1;
+		[SettingIgnore]
+		public int FrogShardCountPos { get; set; } = -1;
 
 		[YamlIgnore]
 		[SettingRange(1, 100)]
 		public int CollectableCountPosition {
 			get {
-				if(frogShardCountPos >= 0) return frogShardCountPos;
+				if(FrogShardCountPos >= 0) return FrogShardCountPos;
 
 				//Determine default value
 				return Everest.Modules.Any(m => m.GetType().FullName.Equals("CelesteDeathTracker.DeathTrackerModule", StringComparison.OrdinalIgnoreCase)) ? 2 : 1;
 			}
-			set => frogShardCountPos = value;
+			set => FrogShardCountPos = value;
 		}
 	}
 }
