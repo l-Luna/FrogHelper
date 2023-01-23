34c354bb05964b450c3facac143b54ade32e6baf
Popax21 <popa21@popa21.at>
2023-01-23 18:21:06 +0100

    Add death tracker detection

--- FrogHelper/FrogHelperSettings.cs
+++ FrogHelper/FrogHelperSettings.cs
@@ -1,8 +1,23 @@
-﻿using Celeste.Mod;
+﻿using System;
+using System.Linq;
+using Celeste.Mod;
+using YamlDotNet.Serialization;
 
 namespace FrogHelper {
 	public class FrogHelperSettings : EverestModuleSettings {
+		[YamlMember]
+		private int frogShardCountPos = -1;
+
+		[YamlIgnore]
 		[SettingRange(1, 100)]
-		public int CollectableCountPosition { get; set; } = 1;
+		public int CollectableCountPosition {
+			get {
+				if(frogShardCountPos >= 0) return frogShardCountPos;
+
+				//Determine default value
+				return Everest.Modules.Any(m => m.GetType().FullName.Equals("CelesteDeathTracker.DeathTrackerModule", StringComparison.OrdinalIgnoreCase)) ? 2 : 1;
+			}
+			set => frogShardCountPos = value;
+		}
 	}
 }
